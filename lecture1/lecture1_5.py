23136f47bdb00011fef7213aee6ee87f374709c0
Vinkourov Alexander <alex@snowworld.ru>
2016-02-18 20:52:52 +0300

    added first homework

--- lecture1/lecture1_5.py
+++ lecture1/lecture1_5.py
@@ -1,16 +1,25 @@
+# -*- coding: utf-8 -*-
+
 import sys
 
-print(sys.version_info)
 
+# which function to use?
 if sys.version_info[0] == 2:
     input_function = raw_input
 else:
     input_function = input
 
+# Infinitive loop:
 while True:
-    user_input = input_function("Please, input positive numbers: ")
-    user_input = float(user_input)
-    if user_input > 0:
-        print("Your number is: %d" % user_input)
+    users_input = input_function('Please, input positive number: ')
+    if float(users_input) > 0:
+        print('Your number is: %s' % users_input)
+        break
     else:
-        print("%d is a wrong number." % user_input)
+        print('%s is a wrong number.' % users_input)
+        continue
+
+
+# Iterating over a chars in the string:
+for char in '12345679':
+    print(int(char) + 100)
